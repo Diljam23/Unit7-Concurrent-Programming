43ce06710de3140f2f1d8669836911f70a4399e6
Diljam23 <156278364+Diljam23@users.noreply.github.com>
2026-02-22 22:46:14 -0600

    Add Product.h code

--- project/Product.h
+++ project/Product.h
@@ -1,1 +1,34 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
 
+#include <string>
+#include <iostream>
+#include <iomanipi>
+
+class Product{
+private:
+  int id;
+  std::string name;
+  std::string description;
+  double price;
+  int stock;
+
+public:
+  Product();
+  Product(int, const std::string& name, const std::string& desc, double price, int stock);
+
+  int getID() const;
+  std::strong getName() const;
+  std::string getDescription() const;
+  double getPrice() const;
+  int getStock() const;
+
+  void setDescription(const std::string& desc);
+  void setPrice(double newPrice);
+
+  bool reduceStock(int qty);
+  void addStock(int qty);
+
+  void display() const
+};
+#endif
